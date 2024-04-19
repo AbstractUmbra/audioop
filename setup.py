914a9903e0fb0eee7b18394e21549387e4336c8e
Sachaa-Thanasius <i.like.ike101@gmail.com>
2024-04-19 11:35:02 -0400

    Initial attempt to make some changes for abi3-compatibility.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import Extension, setup
 
-extensions = [Extension("audioop", ["audioop.c"])]
+extensions = [Extension("audioop", ["audioop.c"], py_limited_api=True, define_macros=[("Py_LIMITED_API", "0x030B0000")])]
 
 setup(ext_modules=extensions)
