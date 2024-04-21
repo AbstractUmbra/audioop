2cdca18dcb132b4e589162ebc60ae9847c64b148
Sachaa-Thanasius <i.like.ike101@gmail.com>
2024-04-21 16:28:58 -0400

    Massive rearrangement. - Put source files in one package. - Add an __init__.py and __init__.pyi to provide a target for type stubs.  - Probably too eager; typeshed will probably provide these until 3.12 is EOL. - Change setup configuration to  a. Put as much as possible in pyproject.toml.  b. Eliminate the need for setup.cfg.  c. Don't break everything when attempting to build with `build` — the audioop header file just wasn't being seen. - Most importantly, make it abi3. i.e., the resulting wheel should be compatible for >=3.12 environments.

--- setup.py
+++ setup.py
@@ -1,5 +1,32 @@
 from setuptools import Extension, setup
 
-extensions = [Extension("audioop", ["audioop.c"], py_limited_api=True, define_macros=[("Py_LIMITED_API", "0x030B0000")])]
+try:
+    from wheel.bdist_wheel import bdist_wheel
+except ImportError:
+    cmdclass = {}
+else:
 
-setup(ext_modules=extensions)
+    class bdist_wheel_abi3(bdist_wheel):
+        def get_tag(self) -> tuple[str, str, str]:
+            python, abi, plat = super().get_tag()
+
+            if python.startswith("cp"):
+                import sys
+
+                return f"cp3{sys.version_info[1]}", "abi3", plat
+
+            return python, abi, plat
+
+    cmdclass = {"bdist_wheel": bdist_wheel_abi3}
+
+extensions = [
+    Extension(
+        name="audioop._audioop",
+        sources=["audioop/_audioop.c"],
+        depends=["audioop/_audioop.c.h"],
+        define_macros=[("Py_LIMITED_API", "0x030C0000")],
+        py_limited_api=True,
+    )
+]
+
+setup(ext_modules=extensions, cmdclass=cmdclass)
