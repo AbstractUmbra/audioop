ec79c01b2c71386a4c2d8460adaf2c7edf26cac8
Sachaa-Thanasius <i.like.ike101@gmail.com>
2024-04-24 10:10:22 -0400

    Implement the limited api and abi3 tag in a less hacky way.
    Source: https://github.com/pypa/wheel/issues/583

--- setup.py
+++ setup.py
@@ -1,35 +1,18 @@
-# pyright: reportUnknownVariableType=none
 from setuptools import Extension, setup
 
-try:
-    from wheel.bdist_wheel import bdist_wheel
-except ImportError:
-    cmdclass = {}
-else:
-
-    class bdist_wheel_abi3(bdist_wheel):
-        def get_tag(self) -> tuple[str, str, str]:
-            python, abi, plat = super().get_tag()
-
-            if python.startswith("cp"):
-                import sys
-
-                return f"cp3{sys.version_info[1]}", "abi3", plat
-
-            return python, abi, plat
-
-    cmdclass = {"bdist_wheel": bdist_wheel_abi3}
+# While the code is compatible with 3.12 because of the Limited API version,
+# install is limited to >=3.13 in package metadata to avoid clobbering the stdlib audioop.
 
 extensions = [
     Extension(
         name="audioop._audioop",
         sources=["audioop/_audioop.c"],
         depends=["audioop/_audioop.c.h"],
-        # Technically compatible with 3.12 because of the Limited API version, but to avoid clobbering,
-        # install is limited to >=3.13 in package metadata.
         define_macros=[("Py_LIMITED_API", "0x030C0000")],
         py_limited_api=True,
     )
 ]
 
-setup(ext_modules=extensions, cmdclass=cmdclass)
+options = {"bdist_wheel": {"py_limited_api": "cp312"}}
+
+setup(ext_modules=extensions, options=options)
