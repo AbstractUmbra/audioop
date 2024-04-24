e1526b6f57c4a36142dd1fd7798e0c6fa18ae76c
Sachaa-Thanasius <i.like.ike101@gmail.com>
2024-04-24 10:16:26 -0400

    Remove a now unnecessary comment.

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import Extension, setup
 
-# While the code is compatible with 3.12 because of the Limited API version,
-# install is limited to >=3.13 in package metadata to avoid clobbering the stdlib audioop.
-
 extensions = [
     Extension(
         name="audioop._audioop",
