b9ee632f330d1e8738f8759ab8c2ffd56004e410
Sachaa-Thanasius <i.like.ike101@gmail.com>
2024-04-24 10:15:38 -0400

    Hardcode the limited api version on the wheel (both the macro and the tag) to be 3.13. - This should be the final necessary measure to avoid clobbering.

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ extensions = [
         name="audioop._audioop",
         sources=["audioop/_audioop.c"],
         depends=["audioop/_audioop.c.h"],
-        define_macros=[("Py_LIMITED_API", "0x030C0000")],
+        define_macros=[("Py_LIMITED_API", "0x030D0000")],
         py_limited_api=True,
     )
 ]
 
-options = {"bdist_wheel": {"py_limited_api": "cp312"}}
+options = {"bdist_wheel": {"py_limited_api": "cp313"}}
 
 setup(ext_modules=extensions, options=options)
