290282c5c354a69156a00cce7d847fa787c491fe
Michael H <michael@michaelhall.tech>
2024-05-08 19:22:38 -0400

    I think this is all that's needed

--- audioop/_audioop.c
+++ audioop/_audioop.c
@@ -1981,6 +1981,7 @@ audioop_exec(PyObject* module)
 static PyModuleDef_Slot audioop_slots[] = {
     {Py_mod_exec, audioop_exec},
     {Py_mod_multiple_interpreters, Py_MOD_PER_INTERPRETER_GIL_SUPPORTED},
+    {Py_mod_gil, Py_MOD_GIL_NOT_USED},
     {0, NULL}
 };
 
