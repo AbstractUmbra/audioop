2e4b8d10c1121ca068607af64a3b064c9f5172a7
Jakub Kuczys <me@jacken.men>
2024-08-02 20:44:25 +0200

    Disable Python limited API in free-threading wheels

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
+import sysconfig
+
 from setuptools import Extension, setup
+
+
+Py_GIL_DISABLED = sysconfig.get_config_var("Py_GIL_DISABLED")
+macros = []
+options = {}
+if not Py_GIL_DISABLED:
+    macros.append(("Py_LIMITED_API", "0x030D0000"))
+    options["bdist_wheel"] = {"py_limited_api": "cp313"}
 
 extensions = [
     Extension(
         name="audioop._audioop",
         sources=["audioop/_audioop.c"],
         depends=["audioop/_audioop.c.h"],
-        define_macros=[("Py_LIMITED_API", "0x030D0000")],
-        py_limited_api=True,
+        define_macros=macros,
+        py_limited_api=not Py_GIL_DISABLED,
     )
 ]
-
-options = {"bdist_wheel": {"py_limited_api": "cp313"}}
 
 setup(ext_modules=extensions, options=options)
